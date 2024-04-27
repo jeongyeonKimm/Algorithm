2d008c572c87c282f6d13c5a6bf4d4047133eba9
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-04-27 23:10:17 +0900

    [level 0] Title: 짝수는 싫어요, Time: 1.81 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,9 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int n) {
+        return IntStream.rangeClosed(1, n)
+            .filter(i -> i % 2 == 1)
+            .toArray();
+    }
+}
