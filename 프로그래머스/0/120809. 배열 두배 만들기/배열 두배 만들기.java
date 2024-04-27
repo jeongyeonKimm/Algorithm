052160973c57837c1a5ac8392cae13939591b87e
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-04-27 22:43:09 +0900

    [level 0] Title: 배열 두배 만들기, Time: 3.03 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        return Arrays.stream(numbers)
+            .map(i -> i * 2)
+            .toArray();
+    }
+}
