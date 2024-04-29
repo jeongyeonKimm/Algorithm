3332966e096d5a682f3641d42b071dba5108daa2
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-04-29 22:53:14 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        for (char c : my_string.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                answer += (int) (c - '0');
+            }
+        }
+        
+        return answer;
+    }
+}
