de66d8b890641078f0f238c8d41c5bcb1590461e
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-04-30 12:34:05 +0900

    [level 0] Title: 대문자와 소문자, Time: 12.17 ms, Memory: 67.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for (char c : my_string.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                answer += Character.toLowerCase(c);
+            } else if (Character.isLowerCase(c)) {
+                answer += Character.toUpperCase(c);
+            }
+        }
+        
+        return answer;
+    }
+}
