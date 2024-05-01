4d054f88728c2770a2c7014d7b3f0dca6ea633dd
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-05-01 10:08:19 +0900

    [level 0] Title: 배열의 유사도, Time: 0.07 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -1,14 +1,15 @@
 import java.util.Arrays;
+import java.util.List;
 
 class Solution {
     public int solution(String[] s1, String[] s2) {
         int answer = 0;
         
+        List<String> list2 = Arrays.asList(s2);
+        
         for (int i = 0; i < s1.length; i++) {
-            for (int j = 0; j < s2.length; j++) {
-                if (s1[i].equals(s2[j])) {
-                    answer++;
-                }
+            if (list2.contains(s1[i])) {
+                answer++;
             }
         }
         
