d462050b205a4ca5b92f6fbd9952fe9b38daed15
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-05-01 10:13:00 +0900

    [level 0] Title: n의 배수 고르기, Time: 1.81 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -1,9 +1,19 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int n, int[] numlist) {
-        return Arrays.stream(numlist)
-            .filter(i -> i % n == 0)
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i : numlist) {
+            if (i % n == 0) {
+                list.add(i);
+            }
+        }
+        
+        return list.stream()
+            .mapToInt(i -> i)
             .toArray();
     }
 }
