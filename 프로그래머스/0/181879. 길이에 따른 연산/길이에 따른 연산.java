a3e5b78c699a92f9a63b85ef511371deaa0b8c7d
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-05-03 22:43:17 +0900

    [level 0] Title: 길이에 따른 연산, Time: 1.10 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] num_list) {
+        if (num_list.length >= 11) {
+            return Arrays.stream(num_list)
+                .sum();
+        } else {
+            return Arrays.stream(num_list)
+                .reduce(1, (a, b) -> a * b);
+        }
+    }
+}
