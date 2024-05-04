d06bd61a026417506f7645e6d69db59cb698a3b0
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-05-04 18:32:37 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> result = new ArrayList<>();
+        
+        for (int i = 0; i < todo_list.length; i++) {
+            if (!finished[i]) {
+                result.add(todo_list[i]);
+            }
+        }
+        
+        return result.toArray(new String[result.size()]);
+    }
+}
