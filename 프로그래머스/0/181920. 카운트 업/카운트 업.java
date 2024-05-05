8cd699ef5ebe314f2048bd8ca7d2bffcb93e094a
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-05-05 18:53:35 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 68 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,9 +1,11 @@
-import java.util.stream.IntStream;
-
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        return IntStream.rangeClosed(start_num, end_num)
-            .map(i -> i)
-            .toArray();
+        int[] result = new int[end_num - start_num + 1];
+        
+        for (int i = start_num; i <= end_num; i++) {
+            result[i - start_num] = i;
+        }
+        
+        return result;
     }
 }
