e48be4233475674e2016f78d083a85eb82802dd0
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-05-05 18:32:00 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        if (my_string.length() >= is_prefix.length() &&
-            my_string.substring(0, is_prefix.length()).equals(is_prefix)) {
+        if (my_string.startsWith(is_prefix)) {
             return 1;
         } else {
             return 0;
