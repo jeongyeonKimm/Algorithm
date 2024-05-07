b65307cade418c01ef12e60fe16e0e459398a7ff
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-05-07 22:46:47 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 0;
         
         for (char c : num_str.toCharArray()) {
-            answer += Integer.parseInt(String.valueOf(c));
+            answer += c - '0';
         }
         return answer;
     }
