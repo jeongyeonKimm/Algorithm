6a1f85cf748db16998f1c73302acb72125763346
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-05-07 22:32:19 +0900

    [level 0] Title: 꼬리 문자열, Time: 1.93 ms, Memory: 66.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        return Arrays.stream(str_list)
+            .filter(s -> !s.contains(ex))
+            .collect(Collectors.joining());
+    }
+}
