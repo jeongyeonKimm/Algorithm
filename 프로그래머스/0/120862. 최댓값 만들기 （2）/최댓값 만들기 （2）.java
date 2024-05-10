7822460fd78511ebf63bbdd44dd8a63b3d48d742
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-05-10 19:44:18 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.54 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        
+        int len = numbers.length;
+        
+        return Math.max(numbers[0] * numbers[1], numbers[len - 2] * numbers[len - 1]);
+    }
+}
