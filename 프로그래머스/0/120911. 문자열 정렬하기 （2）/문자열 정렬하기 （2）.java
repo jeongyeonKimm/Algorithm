04204bbc3c82ef57a9f3cb70e5734e38d31e5988
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-05-15 08:04:37 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.38 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string) {
+        char[] answer = my_string.toLowerCase().toCharArray();
+        
+        Arrays.sort(answer);
+        
+        return new String(answer);
+    }
+}
