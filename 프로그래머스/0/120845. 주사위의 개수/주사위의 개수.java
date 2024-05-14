5975c16e7b50cc4c5eca0cba268843edcb69c5cc
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-05-14 18:54:14 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,5 +1,5 @@
 class Solution {
     public int solution(int[] box, int n) {
-        return (box[0] / n) * (box[1] / n) * (box[2] / n); 
+        return (box[0] / n) * (box[1] / n) * (box[2] / n);
     }
 }
