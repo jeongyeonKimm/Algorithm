c62cc817971a15253c6e6ffb125d632fa4ec1ecc
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-05-15 07:57:22 +0900

    [level 0] Title: 약수 구하기, Time: 2.97 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer.add(i);
+            }
+        }
+        
+        return answer.stream()
+            .mapToInt(i -> i)
+            .toArray();
+    }
+}
