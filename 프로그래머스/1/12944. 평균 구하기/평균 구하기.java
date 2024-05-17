d14dd521f8eafd8fa64446bd2f5b2aff593e7d1f
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-05-18 08:11:23 +0900

    [level 1] Title: 평균 구하기, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        
+        for (int i : arr) {
+            answer += i;
+        }
+        
+        return answer / arr.length;
+    }
+}
