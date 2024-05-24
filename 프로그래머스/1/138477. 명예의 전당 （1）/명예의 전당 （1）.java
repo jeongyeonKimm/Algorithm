16d525a748688efa206e66e253e0cc0531f45eae
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-05-24 20:36:07 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.30 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,16 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        for (int i = 0; i < score.length; i++) {
+            pq.add(score[i]);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+            answer[i] = pq.peek();
+        }
+        return answer;
+    }
+}
