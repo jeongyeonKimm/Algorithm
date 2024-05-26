d787e320318f554f4d31fad3e691aa5ec7f813f3
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-05-26 14:54:43 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (isPrime(nums[i] + nums[j] + nums[k])) answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+    
+    public boolean isPrime(int num) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
