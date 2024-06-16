d7a07ce5e294dd1482307f5b2f41ded34f6e3f32
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-06-17 08:42:56 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,18 +1,19 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[] arr) {
-        Arrays.sort(arr);
-        
-        for (int i = arr[arr.length - 1]; ; i++) {
-            boolean flag = true;
-            for (int n : arr) {
-                if (i % n != 0) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) return i;
+        // A * B = gcd * lcm
+        int lcm = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            int g = gcd(lcm, arr[i]);
+            lcm = (lcm / g) * (arr[i] / g) * g;
         } 
+        
+        return lcm;
+    }
+    
+    private int gcd(int a, int b) {
+        if (a > b) 
+            return (a % b == 0) ? b : gcd(b, a % b); 
+        else 
+            return (b % a == 0) ? a : gcd(a, b % a);
     }
 }
