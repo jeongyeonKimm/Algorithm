67effdc0b5d088c4c03a0a079ceb88255f7bf38c
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-06-27 23:20:09 +0900

    [level 2] Title: 튜플, Time: 2.26 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -8,18 +8,14 @@ class Solution {
         s = s.substring(2, s.length() - 2);
         
         String[] splitted = s.replace("},{", " ").split(" ");
-        Arrays.sort(splitted, new Comparator<String>(){
-            public int compare(String o1, String o2){
-                return Integer.compare(o1.length(), o2.length());
-            }
-        });
+        Arrays.sort(splitted, (o1, o2) -> o1.length() - o2.length());
         
         List<Integer> result = new ArrayList<>();
         for (String str : splitted) {
-            String[] numbers = str.split(",");
-            for (String n : numbers) {
-                if (!result.contains(Integer.parseInt(n))) {
-                    result.add(Integer.parseInt(n));
+            for (String n : str.split(",")) {
+                int number = Integer.parseInt(n);
+                if (!result.contains(number)) {
+                    result.add(number);
                 }
             }
         }
