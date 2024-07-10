ad149214ccfe2699d4f8e29d76999f1516aa37e4
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-07-10 17:53:34 +0900

    [level 2] Title: 땅따먹기, Time: 19.43 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,17 @@
+class Solution {
+    int solution(int[][] land) {
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][1] += Math.max(land[i - 1][0], Math.max(land[i - 1][2], land[i - 1][3]));
+            land[i][2] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][3]));
+            land[i][3] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][2]));
+        }
+        
+        int maxScore = 0;
+        for (int i = 0; i < 4; i++) {
+            maxScore = Math.max(maxScore, land[land.length - 1][i]);
+        }
+        
+        return maxScore;
+    }
+}
