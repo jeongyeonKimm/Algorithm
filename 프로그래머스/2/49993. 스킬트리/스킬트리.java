7350f33cb4ef220b9a8116e8038a67db0a17a649
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-07-13 09:26:12 +0900

    [level 2] Title: 스킬트리, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,48 +1,27 @@
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Stack;
-
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        Map<Character, Integer> skillMap = new HashMap<>();
-
-        for (int i = 0; i < skill.length(); i++) {
-            skillMap.put(skill.charAt(i), i);
-        }
-
         int count = 0;
-        Stack<Character> stack = new Stack<>();
-
+        
         for (String str : skill_trees) {
-            boolean flag = true;
             int idx = 0;
+            boolean flag = true;
             for (char c : str.toCharArray()) {
-                if (skillMap.containsKey(c) && skillMap.get(c) != idx) {
+                int cur = skill.indexOf(c);
+                if (cur == -1) {
+                    continue;
+                }
+                
+                if (cur == idx) {
+                    idx++;
+                } else {
                     flag = false;
                     break;
-                }
-
-                while (!stack.isEmpty() && skillMap.containsKey(c)) {
-                    if (skillMap.get(stack.peek()) > skillMap.get(c)) {
-                        flag = false;
-                        break;
-                    }
-                    stack.pop();
-                }
-
-                if (!flag) break;
-
-                if (skillMap.containsKey(c)) {
-                    stack.push(c);
-                    idx++;
                 }
             }
             
             if (flag) count++;
-            
-            stack.clear();
         }
-
+        
         return count;
     }
 }
