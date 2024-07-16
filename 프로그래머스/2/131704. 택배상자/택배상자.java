26582abad60a4ad57dee182937b7e43bbe5d3fc9
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-07-16 21:33:49 +0900

    [level 2] Title: 택배상자, Time: 69.99 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -10,15 +10,16 @@ class Solution {
         int target = 0; // 현재 트럭에 실어야 하는 우선순위
         Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < priority.length; i++) {
-            if (target == priority[i]) target++;
-            else stack.push(priority[i]);
+            if (target == priority[i]) target++;    // 현재 i번째 우선순위가 찾고자 하는 우선순위와 같으면 트럭에 싣고 찾고자 하는 다음 우선순위로 변경
+            else stack.push(priority[i]);   // 같지 않으면 해당 우선순위를 서브 컨테이너에 넣음
             
-            while (!stack.isEmpty() && target == stack.peek()) {
+            // 찾고자 하는 우선순위와 스택의 top가 같으면 스택에서 꺼내고 다음 우선순위로 변경
+            while (!stack.isEmpty() && target == stack.peek()) {    
                 stack.pop();
                 target++;
             }
         }
         
-        return target;
+        return target;  // 최종 target의 값은 찾은 최종적으로 우선순위
     }
 }
