a747695ad2097c76f43206bb913e567a30f75b02
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-07-25 23:09:14 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 29.50 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -0,0 +1,78 @@
+class Solution {
+    
+    private char[][] map;
+    
+    public int solution(int m, int n, String[] board) {
+        map = new char[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i][j] = board[i].charAt(j);
+            }
+        }
+        
+        int total = 0;
+        
+        while (true) {
+            boolean[][] check = new boolean[m][n];
+            
+            checkMap(check, m, n);
+            
+            int count = countBlock(check, m, n);
+            if (count == 0) break;
+            total += count;
+            
+            moveMap(m, n);
+        }
+        
+        return total;
+    }
+    
+    private void checkMap(boolean[][] check, int m, int n) {
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (map[i][j] == map[i][j + 1] && 
+                    map[i][j] == map[i + 1][j] && 
+                    map[i][j] == map[i + 1][j + 1]) {
+                    check[i][j] = true;
+                    check[i][j + 1] = true;
+                    check[i + 1][j] = true;
+                    check[i + 1][j + 1] = true;
+                }
+            }
+        }
+    }
+    
+    private int countBlock(boolean[][] check,  int m, int n) {
+        int count = 0;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (check[i][j] && map[i][j] != '-') {
+                    count++;
+                    map[i][j] = '-';
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    private void moveMap(int m, int n) {
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                if (map[i][j] != '-') {
+                    continue;
+                }
+                
+                for (int k = i - 1; k >= 0; k--) {
+                    if (map[k][j] != '-') {
+                        map[i][j] = map[k][j];
+                        map[k][j] = '-';
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
