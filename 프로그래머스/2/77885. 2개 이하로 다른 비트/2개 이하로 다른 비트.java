d0b8658f55c494b568a44bfab143e2c5a1ca2c9c
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-07-27 22:33:25 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 2.96 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -3,17 +3,8 @@ class Solution {
         long[] answer = new long[numbers.length];
         
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 == 0) {
-                answer[i] = numbers[i] + 1;
-            } else {
-                String target = Long.toBinaryString(numbers[i]);
-                int index = target.lastIndexOf("0");
-                if (index == -1) {
-                    answer[i] = Long.parseLong("10" + target.substring(1), 2);
-                } else {
-                    answer[i] = Long.parseLong(target.substring(0, index) + "10" + target.substring(index + 2), 2);
-                }
-            }
+            answer[i] = numbers[i] + 1;
+            answer[i] += (answer[i] ^ numbers[i]) >> 2;
         }
         
         return answer;
