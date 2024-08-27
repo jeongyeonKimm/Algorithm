698757489720483282bd29816f795f8c5f658155
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-08-27 22:15:00 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 456 ms, Memory: 71360 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -11,20 +11,21 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
+        sb.append((int) Math.pow(2, n) - 1 + "\n");
+        
         hanoi(n, 1, 2, 3);
-
-        System.out.println((int) Math.pow(2, n) - 1);
+        
         System.out.println(sb.toString());
     }
 
     private static void hanoi(int n, int from, int aux, int to) {
         if (n == 1) {
-            sb.append(from + " " + to).append("\n");
+            sb.append(from + " " + to + "\n");
             return;
         }
 
         hanoi(n - 1, from, to, aux);
-        sb.append(from + " " + to).append("\n");
+        sb.append(from + " " + to + "\n");
         hanoi(n - 1, aux, from, to);
     }
 }
