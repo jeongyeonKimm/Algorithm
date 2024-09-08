30be9bfd0868e9be79e3039135f0298066a9fff5
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-09-08 10:10:31 +0900

    [Gold V] Title: 집합의 표현, Time: 624 ms, Memory: 60140 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            parent[i] = i;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (command == 0) {
+                union(a, b);
+            } else if (command == 1) {
+                if (isSameParent(a, b)) {
+                    sb.append("YES\n");
+                } else {
+                    sb.append("NO\n");
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    static int find(int x) {    // 부모를 찾는 함수
+        if (x == parent[x]) {
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x > y) {
+            parent[x] = y;
+        } else {
+            parent[y] = x;
+        }
+    }
+
+    static boolean isSameParent(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x == y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
