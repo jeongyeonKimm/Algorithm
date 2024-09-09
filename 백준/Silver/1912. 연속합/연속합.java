43eaf1703344fc11f58a20cbbf28840defa112ad
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-09-09 09:26:37 +0900

    [Silver II] Title: 연속합, Time: 224 ms, Memory: 23812 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+
+        dp[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}
