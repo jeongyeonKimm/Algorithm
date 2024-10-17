657d56ab9dd5886f0bf41b56a09f95ef3ac1ff73
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-10-17 21:30:42 +0900

    [Gold V] Title: 사람의 수, Time: 124 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Gold/1206. 사람의 수/사람의 수.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        double[] scores = new double[N];
+
+        for (int i = 0; i < N; i++) {
+            double score = Double.parseDouble(br.readLine());
+            score = score - Math.floor(score);
+            scores[i] = Math.round(score * 1000.0) / 1000.0;    // 입력 받은 각 문항의 평균 점수를 소수점 부분만 남김
+        }
+
+        double[] avg = new double[1001];
+
+        for (int i = 1; i < 1001; i++) {    // i는 가능한 사람 수. 1 ~ 1000(입력된 평균 점수가 소수점 셋째 자리까지 표현되기 때문에 가능한 사람수의 최대는 1000)
+            for (int j = 1; j <= i; j++) {  // j는 j번째 문제에서 가능한 i명의 사람드의 총 점수의 합 범위(1 ~ 사람 수). 입력을 소수점 부분만 받아서 그 이상은 의미 X.
+                avg[j] = Math.floor(((double) j / i) * 1000) / 1000.0;
+            }
+
+            boolean flag = true;
+            for (int j = 0; j < N; j++) {
+                // 입력받은 N개의 문항의 평균 값이 모두 있는지 binarySearch로 확인.(i명의 사람의 평균이라는 의미) 하나라도 없으면 i명이 아님.
+                if (Arrays.binarySearch(avg, 0, i + 1, scores[j]) < 0) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                System.out.println(i);
+                return;
+            }
+        }
+    }
+}
