d25ea05cf0aee893b5dec9e6475db6386c3bf27a
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-10-27 10:30:45 +0900

    [Silver I] Title: 단축키 지정, Time: 112 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/1283. 단축키 지정/단축키 지정.java
+++ 백준/Silver/1283. 단축키 지정/단축키 지정.java
@@ -1,65 +1,93 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
+    static Set<Character> usedShortCuts;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
-        boolean[] isUsed = new boolean[26];
+        usedShortCuts = new HashSet<>();
 
         StringBuilder answer = new StringBuilder();
 
         for (int i = 0; i < N; i++) {
             String option = br.readLine();
-            String input = option.toLowerCase();
-            String[] words = input.split(" ");
+            String[] words = option.split(" ");
 
-            int repIdx = -1;
-            int tmp = 0;
-            for (int j = 0; j < words.length; j++) {
-                int idx = words[j].charAt(0) - 'a';
-                if (!isUsed[idx]) {
-                    isUsed[idx] = true;
-                    repIdx = tmp;
-                    break;
-                }
-                tmp += words[j].length();
-                tmp++;  // 공백 count
-            }
-
-            if (repIdx > -1) {
-                answer.append(option.substring(0, repIdx) + "[" + option.charAt(repIdx)
-                        + "]" + option.substring(repIdx + 1) + "\n");
+            String result = checkFirstLetter(words);
+            if (result != null) {
+                answer.append(result).append("\n");
                 continue;
             }
 
-            for (int j = 1; j < input.length(); j++) {
-                int idx = input.charAt(j) - 'a';
-
-                if (option.charAt(j) == ' ') {
-                    j++;
-                    continue;
-                }
-
-                if (!isUsed[idx]) {
-                    isUsed[idx] = true;
-                    repIdx = j;
-                    break;
-                }
+            result = checkOtherLetters(words);
+            if (result != null) {
+                answer.append(result).append("\n");
+                continue;
             }
 
-            if (repIdx > -1) {
-                answer.append(option.substring(0, repIdx) + "[" + option.charAt(repIdx)
-                        + "]" + option.substring(repIdx + 1) + "\n");
-            } else {
-                answer.append(option + "\n");
-            }
+            answer.append(option).append("\n");
         }
 
         System.out.println(answer);
+    }
+
+    private static String checkFirstLetter(String[] words) {
+        for (int i = 0; i < words.length; i++) {
+            char first = words[i].toLowerCase().charAt(0);
+
+            if (!usedShortCuts.contains(first)) {
+                usedShortCuts.add(first);
+                return makeStringWithBracket(words, i, 0);
+            }
+        }
+        return null;
+    }
+
+    private static String checkOtherLetters(String[] words) {
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            for (int j = 0; j < word.length(); j++) {
+                char letter = word.toLowerCase().charAt(j);
+
+                if (!usedShortCuts.contains(letter)) {
+                    usedShortCuts.add(letter);
+                    return makeStringWithBracket(words, i, j);
+                }
+            }
+        }
+        return null;
+    }
+
+
+    private static String makeStringWithBracket(String[] words, int wordIdx, int letterIdx) {
+        StringBuilder newStr = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+
+            if (i > 0) {
+                newStr.append(" ");
+            }
+            
+            if (i == wordIdx) {
+                newStr.append(word.substring(0, letterIdx))
+                        .append("[")
+                        .append(word.charAt(letterIdx))
+                        .append("]")
+                        .append(word.substring(letterIdx + 1));
+            } else {
+                newStr.append(word);
+            }
+        }
+        
+        return newStr.toString();
     }
 }
