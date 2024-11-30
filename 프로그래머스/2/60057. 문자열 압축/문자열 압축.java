707252c8ac2ec82f1f1926e99344d878bbb16e78
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-11-30 19:53:26 +0900

    [level 2] Title: 문자열 압축, Time: 10.72 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -3,32 +3,26 @@ class Solution {
         int minLength = s.length();
         
         for (int i = 1; i <= s.length() / 2; i++) {
-            StringBuilder sb = new StringBuilder();
-            int count = 1;
-            String prev = s.substring(0, i);
-            for (int j = i; j < s.length(); j += i) {
-                int endIdx = Math.min(j + i, s.length());
-                String tmp = s.substring(j, endIdx);
-                if (tmp.equals(prev)) {
-                    count++;
-                } else {
-                    if (count > 1) {
-                        sb.append(count);
-                    }
-                    sb.append(prev);
-                    count = 1;
-                    prev = tmp;
-                }
-            }
-            
-            if (count > 1) {
-                sb.append(count);
-            }
-            sb.append(prev);
-
-            minLength = Math.min(minLength, sb.length());
+            String splitted = getSplittedString(s, i, 1).toString();
+            minLength = Math.min(minLength, splitted.length());
         }
         
         return minLength;
+    }
+    
+    public StringBuilder getSplittedString(String str, int splittedLen, int count) {
+        if (str.length() < splittedLen) return new StringBuilder(str);
+        
+        String pre = str.substring(0, splittedLen);
+        String post = str.substring(splittedLen, str.length());
+        StringBuilder result = new StringBuilder();
+        
+        if (!post.startsWith(pre)) {
+            if (count > 1) result.append(count);
+            result.append(pre);
+            return result.append(getSplittedString(post, splittedLen, 1));
+        }
+        
+        return result.append(getSplittedString(post, splittedLen, count + 1));
     }
 }
