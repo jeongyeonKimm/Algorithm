ff8f0e4a5ad6dd95c148d2877ef5ff70ae8d7581
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-12-03 19:18:04 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 536 ms, Memory: 50016 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+
+        ArrayList<Edge> edges = new ArrayList<>();
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            edges.add(new Edge(a, b, c));
+        }
+
+        parent = new int[v + 1];
+        for (int i = 0; i < v + 1; i++) {
+            parent[i] = i;
+        }
+
+        edges.sort(Comparator.comparingInt(o -> o.cost));
+
+        long answer = 0;
+        for (int i = 0; i < e; i++) {
+            Edge edge = edges.get(i);
+            if (findParent(edge.v1) != findParent(edge.v2)) {
+                unionParent(edge.v1, edge.v2);
+                answer += edge.cost;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    private static int findParent(int x) {
+        if (parent[x] != x) {
+            parent[x] = findParent(parent[x]);
+        }
+        return parent[x];
+    }
+
+    private static void unionParent(int a, int b) {
+        a = findParent(a);
+        b = findParent(b);
+
+        if (a < b) {
+            parent[b] = a;
+        } else {
+            parent[a] = b;
+        }
+    }
+
+    static class Edge {
+
+        int v1;
+        int v2;
+        int cost;
+
+        public Edge(int v1, int v2, int cost) {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.cost = cost;
+        }
+    }
+}
