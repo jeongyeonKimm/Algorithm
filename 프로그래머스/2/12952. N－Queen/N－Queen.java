499b36b4bff68ca4d20b0f2320c7609d5b7e26ad
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-12-15 10:10:36 +0900

    [level 2] Title: N-Queen, Time: 161.61 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -4,35 +4,35 @@ class Solution {
     private int result = 0;
     
     public int solution(int n) {
-        board = new int[n];
+        board = new int[n]; // (i, board[i])에 queen 위치
         
         backtracking(0, n);
         
         return result;
     }
     
-    private void backtracking(int col, int n) {
-        if (col == n) {
+    private void backtracking(int row, int n) {
+        if (row == n) {
             result++;
             return;
         }
         
         for (int i = 0; i < n; i++) {
-            board[col] = i;
+            board[row] = i;
             
-            if (isPossible(col, n)) {
-                backtracking(col + 1, n);
+            if (isPossible(row, n)) {
+                backtracking(row + 1, n);
             }
         }
     }
     
-    private boolean isPossible(int col, int n) {
-        for (int i = 0; i < col; i++) {
-            if (board[col] == board[i]) {
+    private boolean isPossible(int row, int n) {
+        for (int i = 0; i < row; i++) {
+            if (board[row] == board[i]) {   // 같은 세로줄에 위치
                 return false;
             }
             
-            if (Math.abs(col - i) == Math.abs(board[col] - board[i])) {
+            if (Math.abs(row - i) == Math.abs(board[row] - board[i])) { // 대각선에 위치(대각선은 가로와 세로 각각의 절대값이 같음)
                 return false;
             }
         }
