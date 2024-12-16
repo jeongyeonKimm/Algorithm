4622114bf87d9e0ebafcd9671c413b5851d0406f
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-12-16 22:26:46 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 28.69 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -1,28 +1,14 @@
 class Solution {
     public long solution(int r1, int r2) {
-        return countIntegerPair(r2) - countIntegerPair(r1) + countRadius(r1);
-    }
-    
-    private long countIntegerPair(int radius) {
-        long count = 0;
+        long answer = 0;
         
-        for (int i = 0; i <= radius; i++) {
-            count += (int) Math.floor(Math.sqrt(Math.pow(radius, 2) - Math.pow(i, 2)));
+        for (int i = 1; i <= r2; i++) {
+            int count1 = (int) Math.ceil(Math.sqrt(Math.pow(r1, 2) - Math.pow(i, 2)));
+            int count2 = (int) Math.floor(Math.sqrt(Math.pow(r2, 2) - Math.pow(i, 2)));
+            
+            answer += count2 - count1 + 1;
         }
         
-        return 4 * count + 1;
-    }
-    
-    private long countRadius(int radius) {
-        long count = 0;
-        
-        for (int i = 1; i < radius; i++) {
-            int len = (int) Math.floor(Math.sqrt(Math.pow(radius, 2) - Math.pow(i, 2)));
-            if (Math.pow(radius, 2) == Math.pow(i, 2) + Math.pow(len, 2)) {
-                count++;
-            }
-        }
-        
-        return count * 4 + 4;
+        return answer * 4;
     }
 }
