2881a3766dc8db76c77fe5dcac0ec24bc5b5dc17
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-12-19 22:29:23 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.57 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 class Solution {
     
-    private boolean[] opened;
-    private int count;
-    
     public int solution(int[] cards) {
         int n = cards.length;
         
-        opened = new boolean[n + 1];
+        boolean[] opened = new boolean[n];
         List<Integer> scores = new ArrayList<>();
         
         for (int i = 0; i < n; i++) {
-            if (!opened[i + 1]) {
-                count = 1;
-                opened[i + 1] = true;
-                dfs(cards, cards[i]);
-                scores.add(count);
+            int num = i;
+            int count = 0;
+            
+            while (!opened[num]) {
+                count++;
+                opened[num] = true;
+                num = cards[num] - 1;
             }
+            scores.add(count);
         }
         
         if (scores.size() < 2) return 0;
@@ -25,13 +25,5 @@ class Solution {
         Collections.sort(scores, Collections.reverseOrder());
         
         return scores.get(0) * scores.get(1); 
-    }
-    
-    private void dfs(int[] cards, int num) {
-        if (!opened[num]) {
-            opened[num] = true;
-            count++;
-            dfs(cards, cards[num - 1]);
-        }
     }
 }
