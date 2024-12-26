0ca8e4a293428190cacfabbd05e9b48fe5d9c4de
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2024-12-26 22:43:41 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 13.84 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -2,19 +2,20 @@ class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
         
-        int d = 0;
-        int p = 0;
+        int d = 0;  // 한번 이동할 때 배달 가능한 상자의 최대 개수
+        int p = 0;  // 한번 이동할 때 수거 가능한 상자의 최대 개수
         
         for (int i = n - 1; i >= 0; i--) {
             if (deliveries[i] > 0 || pickups[i] > 0) {
-                int cnt = 0;
+                int cnt = 0;    // i번째 집을 방문하는 횟수
                 
                 while (d < deliveries[i] || p < pickups[i]) {
                     cnt++;
-                    d += cap;
+                    d += cap;   // 한번 이동할 때 배달 및 수거는 최대 cap 만큼 가능
                     p += cap;
                 }
                 
+                // 현재 집까지 이동하는데 배달 및 수거 가능한 상자의 개수 범위 내에서 물류센터로 다시 이동하지 않고 배달 및 수거 가능한 상자의 개수 차감
                 d -= deliveries[i];
                 p -= pickups[i];
                 answer += (i + 1) * cnt * 2;
