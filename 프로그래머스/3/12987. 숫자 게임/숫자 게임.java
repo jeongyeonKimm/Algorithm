2959fbccf03ccf4a2c3ed45bfff2cd0fa2a30158
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2025-01-03 22:22:48 +0900

    [level 3] Title: 숫자 게임, Time: 141.92 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -2,31 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] A, int[] B) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int a : A) {
-            pq.offer(a);
-        }
-        
+        Arrays.sort(A);
         Arrays.sort(B);
         
-        Deque<Integer> deque = new ArrayDeque<>();
-        for (int b : B) {
-            deque.add(b);
-        }
-        
-        // A >= B : 승점 X -> B에 있는 것 중 가장 작은 값 출전
-        // A < B : 승점 O
         int score = 0;
         
-        while (!deque.isEmpty()) {
-            int a = pq.poll();
-            int b = deque.peekLast();
-            
-            if (a < b) {
+        for (int i = A.length - 1, j = B.length - 1; i >= 0; i--) {
+            if (A[i] < B[j]) {
                 score++;
-                deque.pollLast();
-            } else {
-                deque.pollFirst();
+                j--;
             }
         }
         
