372c1323cd7b8afea233f7c35f9663a62a05ab78
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2025-06-30 10:37:50 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 8.16 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/README.md
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/README.md
@@ -1,10 +1,10 @@
 # [level 2] 메뉴 리뉴얼 - 72411 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72411) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72411?language=java) 
 
 ### 성능 요약
 
-메모리: 81.8 MB, 시간: 5.18 ms
+메모리: 79 MB, 시간: 8.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 05일 13:02:51
+2025년 06월 30일 10:37:46
 
 ### 문제 설명
 
--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,44 +1,34 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.ArrayList;
 
 class Solution {
     
     Map<String, Integer> map;
     
     public String[] solution(String[] orders, int[] course) {
-        // 최종 코스요리 메뉴가 오름차순으로 정렬되어 있어야 하기 때문에 각 손님들의 단품메뉴 오름차순 정렬
         for (int i = 0; i < orders.length; i++) {
-            char[] order = orders[i].toCharArray();
-            Arrays.sort(order);
-            orders[i] = String.valueOf(order);
-        }
+            char[] menus = orders[i].toCharArray();
+            Arrays.sort(menus);
+            orders[i] = String.valueOf(menus);
+        } 
         
-        ArrayList<String> result = new ArrayList<>();
+        List<String> result = new ArrayList<>();
         
-        for (int i = 0; i < course.length; i++) {
+        for (int c : course) {
             map = new HashMap<>();
-            StringBuilder sb = new StringBuilder();
             
-            for (int j = 0; j < orders.length; j++) {
-                if (course[i] <= orders[j].length()) {
-                    makeCourse(sb, orders[j], 0, 0, course[i]);
-                }
+            for (String order : orders) {
+                makeCourse(order, c, new StringBuilder(), 0, 0);
             }
             
-            int max = 0;    // 가장 많이 주문된 조합의 주문 횟수
-            
+            int maxCount = 0;
             for (Entry<String, Integer> entry : map.entrySet()) {
-                max = Math.max(max, entry.getValue());    
+                maxCount = Math.max(maxCount, entry.getValue());
             }
             
-            if (max >= 2) {
+            if (maxCount >= 2) {
                 for (Entry<String, Integer> entry : map.entrySet()) {
-                    if (entry.getValue() == max) {
+                    if (entry.getValue() == maxCount) {
                         result.add(entry.getKey());
                     }
                 }
@@ -50,16 +40,16 @@ class Solution {
         return result.toArray(new String[result.size()]);
     }
     
-    public void makeCourse(StringBuilder sb, String order, int start, int depth, int n) {
-        if (depth == n) {
-            map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + 1);
+    public void makeCourse(String order, int courseSize, StringBuilder courseMenu, int start, int depth) {
+        if (depth == courseSize) {
+            map.put(courseMenu.toString(), map.getOrDefault(courseMenu.toString(), 0) + 1);
             return;
         }
         
         for (int i = start; i < order.length(); i++) {
-            sb.append(order.charAt(i));
-            makeCourse(sb, order, i + 1, depth + 1, n);
-            sb.delete(depth, depth + 1);   
+            courseMenu.append(order.charAt(i));
+            makeCourse(order, courseSize, courseMenu, i + 1, depth + 1);
+            courseMenu.delete(depth, depth + 1);
         }
     }
 }
