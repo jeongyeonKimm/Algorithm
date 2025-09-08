3694f08231ff3f0e014286a5ba39742ac577a6d5
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2025-09-09 00:01:32 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -5,11 +5,11 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int[][] map;
-    static int n, m;
-    static int count = 1;
+    private static int[] dx = {-1, 0, 1, 0};
+    private static int[] dy = {0, 1, 0, -1};
+    private static int n, m, r, c, d;
+    private static int[][] map;
+    private static int result = 1;  // 첫 칸은 무조건 청소
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,14 +17,12 @@ public class Main {
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
 
         st = new StringTokenizer(br.readLine());
-
-        int r = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int d = Integer.parseInt(st.nextToken());
-
-        map = new int[n][m];
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -33,39 +31,36 @@ public class Main {
             }
         }
 
-        clean(r, c, d);
+        cleaning(r, c, d);
 
-        System.out.println(count);
+        System.out.println(result);
     }
 
-    static void clean(int x, int y, int direction) {
+    private static void cleaning(int x, int y, int d) {
         map[x][y] = -1;
 
         for (int i = 0; i < 4; i++) {
-            direction = (direction + 3) % 4;
-
-            int nx = x + dx[direction];
-            int ny = y + dy[direction];
+            d = (d + 3) % 4;    // 반시계 90도 회전
+            int nx = x + dx[d];
+            int ny = y + dy[d];
 
             if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
                 continue;
             }
 
             if (map[nx][ny] == 0) {
-                count++;
-                clean(nx, ny, direction);
+                result++;
+                cleaning(nx, ny, d);
                 return;
             }
         }
 
-        int back = (direction + 2) % 4;
-        int bx = x + dx[back];
-        int by = y + dy[back];
+        int back = (d + 2) % 4;
+        int bx = x + dx[back]; // 후진했을 때 위치
+        int by = y + dy[back]; // 후진했을 때 위치
 
-        if (bx >= 0 && bx < n && by >= 0 && by < m) {
-            if (map[bx][by] != 1) {
-                clean(bx, by, direction);
-            }
+        if (bx >= 0 && bx < n && by >= 0 && by < m && map[bx][by] != 1) {
+            cleaning(bx, by, d); // 후진해도 방향은 유지
         }
     }
 }
