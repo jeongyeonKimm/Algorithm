6c3b8010a985d3246fea67244402c32331a4576c
Jeongyeon Kim <97280492+jeongyeonKimm@users.noreply.github.com>
2025-09-15 23:51:30 +0900

    [Gold IV] Title: 연구소, Time: 824 ms, Memory: 300444 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -0,0 +1,114 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int[] dx = {0, 0, -1, 1};
+    private static int[] dy = {1, -1, 0, 0};
+    private static int n, m;
+    private static int[][] map, copyMap;
+    private static int result = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfs(0);
+
+        System.out.println(result);
+    }
+
+    private static void dfs(int count) {
+        if (count == 3) {
+            bfs();
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 0) {
+                    map[i][j] = 1;
+                    dfs(count + 1);
+                    map[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    private static void bfs() {
+        Queue<Point> queue = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 2) {
+                    queue.add(new Point(i, j));
+                }
+            }
+        }
+
+        copyMap = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            copyMap[i] = map[i].clone();
+        }
+
+        while (!queue.isEmpty()) {
+            Point point = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = point.x + dx[i];
+                int ny = point.y + dy[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                    continue;
+                }
+
+                if (copyMap[nx][ny] == 0) {
+                    copyMap[nx][ny] = 2;
+                    queue.add(new Point(nx, ny));
+                }
+            }
+        }
+
+        countSafeZone();
+    }
+
+    private static void countSafeZone() {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (copyMap[i][j] == 0) {
+                    count++;
+                }
+            }
+        }
+
+        result = Math.max(result, count);
+    }
+
+    static class Point {
+
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
